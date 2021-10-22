4891eb7ea016cede6be79e4dfa4a55ada4d736c3
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-22 10:00:26 -0300

    run compiler

--- dist/data/index.js
+++ dist/data/index.js
@@ -11,7 +11,7 @@ const tigersId = 'e8481c1d-42ea-4610-8e11-1752cfc05a46';
 const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
 const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
 const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
-const animals = {
+const data = {
     animals: [
         {
             id: lionId,
@@ -334,4 +334,4 @@ const animals = {
         Child: 20.99,
     },
 };
-exports.default = animals;
+exports.default = data;
--- dist/functions/animalsOlderThan.js
+++ dist/functions/animalsOlderThan.js
@@ -1,13 +1,10 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const data_1 = __importDefault(require("../data"));
-const { animals } = data_1.default;
-function animalsOlderThan({ animalName, ageAnimal }) {
+;
+;
+function animalsOlderThan({ animalName, ageAnimal }, data) {
     var _a;
-    return (_a = animals.find(({ name }) => name === animalName)) === null || _a === void 0 ? void 0 : _a.residents.every(({ age }) => age >= ageAnimal);
+    return (_a = data.animals.find(({ name }) => name === animalName)) === null || _a === void 0 ? void 0 : _a.residents.every(({ age }) => age >= ageAnimal);
 }
-console.log(animalsOlderThan({ animalName: 'lions', ageAnimal: 5 }));
+;
 exports.default = animalsOlderThan;
