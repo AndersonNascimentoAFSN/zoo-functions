129c6e6e7b51fe61d6df9b96203424fa67572c80
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-22 09:58:09 -0300

    add typescript

--- src/functions/animalsByIds.ts
+++ src/functions/animalsByIds.ts
@@ -1,8 +1,17 @@
-import data from '../data';
+interface IAnimals {
+  animals: [
+    ...{
+      id: string,
+      name: string,
+      popularity: number,
+      location: string,
+      residents: [...{ name: string, sex: string, age: number}[]],
+    }[]
+    ]
+};
 
-function animalsByIds(...ids: Array<string>) {
-  const { animals } = data;
-  return animals.filter(({ id }: any) => ids.includes(id));
+function animalsByIds(data: IAnimals, ...ids: Array<string>) {
+  return data.animals.filter(({ id }: any) => ids.includes(id));
 }
 
 export default animalsByIds;
